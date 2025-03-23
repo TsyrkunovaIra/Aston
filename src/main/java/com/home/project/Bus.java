53cd5ab068e6f581a3c933412ff054c5170ba35a
TsyrkunovaIra <157691083+TsyrkunovaIra@users.noreply.github.com>
2025-03-23 20:20:59 +0300

    builder bus, student, user

---
+++ src/main/java/com/home/project/Bus.java
@@ -0,0 +1,56 @@
+package com.home.project;
+
+public class Bus {
+    private  int number;
+    private  String model;
+    private long mileage;
+    private int year;
+
+    public int getNumber() {
+        return number;
+    }
+    public String getModel() {
+        return model;
+    }
+    public long getMileage() {
+        return mileage;
+    }
+    public void setMileage(long mileage) {
+        this.mileage = mileage;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    @Override
+    public String toString() {
+        return "Bus number" + number + " ,model " + model + " ,mileage " + mileage;
+    }
+    public static class BusBuilder {
+        private final Bus newBus;
+        public BusBuilder () {
+           newBus = new Bus();
+        }
+       public BusBuilder withNumber(int number){
+            newBus.number = number;
+            return this;
+       }
+       public BusBuilder withModel(String model){
+            newBus.model = model;
+            return this;
+       }
+       public BusBuilder withMileage(long mileage){
+            newBus.mileage = mileage;
+            return this;
+       }
+        public BusBuilder withYear(int year){
+            newBus.year = year;
+            return this;
+        }
+        public Bus buidBus(){
+            return newBus;
+        }
+    }
+}
---
+++ src/main/java/com/home/project/User.java
@@ -0,0 +1,62 @@
+package com.home.project;
+
+
+public class User {
+    private String name;
+    private  String password;
+    private String email;
+    private int year;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    @Override
+    public String toString(){
+        return "Name " + name + " ,password " + password + " ,email " + email;}
+    public static class UserBuilder {
+        private final User newUser;
+        public UserBuilder () {
+            newUser = new User();
+        }
+        public UserBuilder withGroup(String name) {
+            newUser.name = name;
+            return this;
+        }
+        public UserBuilder withPassword(String password) {
+            newUser.password = password;
+            return this;
+        }
+        public UserBuilder withEmail(String email) {
+            newUser.email = email;
+            return this;
+        }
+        public UserBuilder withYear(int year) {
+            newUser.year = year;
+            return this;
+        }
+        public User buidUser() {
+            return newUser;
+        }
+    }
+}
+
